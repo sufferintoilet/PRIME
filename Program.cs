93ddf5da34769f3a3f9b3fd4ad75709f2ec366cb
sufferintoilet <nguyenhuynhquocan1@gmail.com>
2023-12-19 21:48:07 +0900

    PRIME2

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("enter a number");
+        int number = Convert.ToInt32(Console.ReadLine());
+        if(number<5)
+        {
+            Console.WriteLine("khong phai so nguyen to");
+        }
+        else
+        {
+            int i=5;
+            bool check=true;
+            while (i<=Math.Sqrt(number)) 
+            {
+                if(number%1==0) 
+                {
+                check = false;
+                break; 
+                }
+                i++;
+        }
+        if(check)
+        {
+
+        }
+        else
+        {
+
+        }
+}
+}
+}
